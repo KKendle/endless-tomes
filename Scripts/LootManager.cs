5bd830afded3b70231dfdcd2f1345b5a80d6c39e
Kevin Kendle <kevin.kendle09@gmail.com>
2018-09-06 20:24:31 -0500

    placeholders for future loot drops

--- Scripts/LootManager.cs
+++ Scripts/LootManager.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class LootManager : MonoBehaviour {
 
-	private bool isWeaponDrop = true;
-	private bool isArmorDrop = true;
+	private bool isWeaponDrop = false;
+	private bool isArmorDrop = false;
 	private string randomElement;
 	private string randomMaterial;
 	private string randomType;
@@ -13,31 +13,45 @@ public class LootManager : MonoBehaviour {
 	private string generatedWeapon;
 	private bool presetWeapon;
 	private float presetWeaponChance = 10.0f;
-	private float presetWeaponChanceResult;
+	private bool presetArmor;
+	private float presetArmorChance = 10.0f;
 
 	public void enemyDrops() {
         // Debug.Log("checking for enemy drops");
 		WeaponGenerator weaponGenerator = gameObject.GetComponent<WeaponGenerator>();
 		ArmorGenerator armorGenerator = gameObject.GetComponent<ArmorGenerator>();
 
-		// check for preset weapon chance
-		checkPresetWeapon();
+		checkItemDrop();
 
 		if (isWeaponDrop) {
+			// check for preset weapon chance
+			checkPresetWeapon();
+
 			weaponGenerator.generateWeapon(presetWeapon);
 			isWeaponDrop = false;
 		}
 
 		if (isArmorDrop) {
+			// check for preset armor chance
+			checkPresetArmor();
+
 			armorGenerator.generateArmor();
 			isArmorDrop = false;
 		}
     }
 
+	// need to figure out return type
+	// and how I'll set weapon/armor bool's
+	// can have factors based on enemy difficulty
+	private void checkItemDrop() {
+		Debug.Log("checking chance of item drops");
+
+	}
+
 	private bool checkPresetWeapon() {
 		Debug.Log("checking chance of preset weapon");
 		Debug.Log("chance of getting Preset Weapon is " + presetWeaponChance);
-		presetWeaponChanceResult = Random.Range(0.0f, 100.0f);
+		float presetWeaponChanceResult = Random.Range(0.0f, 100.0f);
 		Debug.Log("outcome is " + presetWeaponChanceResult + ".");
 
 		if (presetWeaponChanceResult <= 10.0f) {
@@ -49,5 +63,22 @@ public class LootManager : MonoBehaviour {
 		}
 
 		return presetWeapon;
+	}
+
+	private bool checkPresetArmor() {
+		Debug.Log("checking chance of preset armor");
+		Debug.Log("chance of getting Preset Armor is " + presetArmorChance);
+		float presetArmorChanceResult = Random.Range(0.0f, 100.0f);
+		Debug.Log("outcome is " + presetArmorChanceResult + ".");
+
+		if (presetArmorChanceResult <= 10.0f) {
+			Debug.Log("10 or less. Getting preset armor");
+			presetArmor = true;
+		}
+		else {
+			presetArmor = false;
+		}
+
+		return presetArmor;
 	}
 }
